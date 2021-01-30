2fa42034f23f4652b99c6c94ccc2f6ced192e015
Zach Miller <zach@zjmil.org>
2021-01-30 14:41:59 -0500

    cleanup

--- avro.c
+++ avro.c
@@ -2,9 +2,6 @@
 #include <sqlite3ext.h>
 SQLITE_EXTENSION_INIT1
 
-#include <stdbool.h>
-#include <stddef.h>
-
 #include <avro.h>
 
 #define UNUSED(x) (void)(x)
@@ -44,52 +41,22 @@ static void *avroSqliteAllocator(void *user_data, void *ptr, size_t osize, size_
     return sqlite3_realloc64(ptr, nsize);
 }
 
-static void avroSetCustomAllocatorSqlite()
-{
-    avro_set_allocator(avroSqliteAllocator, NULL);
-}
-
-/* sqlite doesn't provide these, so we do */
-static void *sqlite3_calloc(int size)
-{
-    void *p = sqlite3_malloc(size);
-    if (p) {
-        memset(p, 0, size);
-    }
-    return p;
-}
-
-static void *sqlite3_calloc64(sqlite3_uint64 size)
-{
-    void *p = sqlite3_malloc64(size);
-    if (p) {
-        memset(p, 0, size);
-    }
-    return p;
-}
-
-static int avroTypeToSqliteType(avro_schema_t schema, const char **sqliteType, char **pzErr)
+static const char *avroTypeToSqliteType(avro_schema_t schema)
 {
     switch (avro_typeof(schema)) {
-        // really these should be "not null"
         case AVRO_STRING:
         case AVRO_ENUM:
-            *sqliteType = "text";
-            break;
+            return "text";
         case AVRO_BYTES:
-            *sqliteType = "blob";
-            break;
+            return "blob";
         case AVRO_INT32:
         case AVRO_INT64:
-            *sqliteType = "int";
-            break;
+            return "int";
         case AVRO_FLOAT:
         case AVRO_DOUBLE:
-            *sqliteType = "real";
-            break;
+            return "real";
         case AVRO_BOOLEAN:
-            *sqliteType = "boolean";
-            break;
+            return "boolean";
         // case AVRO_NULL:
         // case AVRO_RECORD:
         // case AVRO_FIXED:
@@ -97,68 +64,76 @@ static int avroTypeToSqliteType(avro_schema_t schema, const char **sqliteType, c
         // case AVRO_ARRAY:
         // case AVRO_UNION:
         default:
-            *pzErr = sqlite3_mprintf("Unsupported Avro type: %s", avro_schema_type_name(schema));
-            return SQLITE_ERROR;
+            return NULL;
+    }
+}
+
+typedef struct AvroTable {
+    sqlite3_vtab base;
+    char *zFile;
+    avro_schema_t schema;
+    avro_value_iface_t *iface;
+} AvroTable;
+
+static void avroTableSetError(AvroTable *pAvro, const char *fmt, ...)
+{
+    sqlite3_free(pAvro->base.zErrMsg);
+
+    va_list ap;
+    va_start(ap, fmt);
+    pAvro->base.zErrMsg = sqlite3_vmprintf(fmt, ap);
+    va_end(ap);
+}
+
+static int avroDisconnect(sqlite3_vtab *pVtab)
+{
+    AvroTable *pAvro = (AvroTable *)pVtab;
+
+    if (pAvro) {
+        avro_value_iface_decref(pAvro->iface);
+        avro_schema_decref(pAvro->schema);
+        sqlite3_free(pAvro->zFile);
+        sqlite3_free(pAvro);
     }
 
     return SQLITE_OK;
 }
 
-typedef struct AvroTable {
-    sqlite3_vtab base;
-    char *zDb;
-    char *zName;
-    char *zFile;
-    avro_schema_t schema;
-    avro_value_iface_t *valueIface;
-    avro_file_reader_t reader;
-    avro_value_t value;
-    bool eof;
-} AvroTable;
-
-typedef struct AvroCursor {
-    sqlite3_vtab_cursor base;
-    int64_t row;
-} AvroCursor;
-
-void avroTableFree(AvroTable *pAvro)
-{
-    avro_value_iface_decref(pAvro->valueIface);
-    avro_schema_decref(pAvro->schema);
-    avro_file_reader_close(pAvro->reader);
-    free(pAvro);
-}
-
-int avroCreate(sqlite3 *db, void *pAux, int argc, const char *const*argv, sqlite3_vtab **ppVtab, char **pzErr)
+static int avroCreate(sqlite3 *db, void *pAux, int argc, const char *const*argv, sqlite3_vtab **ppVtab, char **pzErr)
 {
     UNUSED(pAux);
 
     int rc = SQLITE_OK;
+    AvroTable *pAvro = NULL;
+    char *errMsg = NULL;
+    char *createTable = NULL;
+    avro_file_reader_t reader = NULL;
 
     if (argc < 4) {
-        *pzErr = sqlite3_mprintf("No Avro file specified");
-        return SQLITE_ERROR;
+        errMsg = sqlite3_mprintf("No Avro file specified");
+        rc = SQLITE_ERROR;
+        goto error;
     }
 
-    size_t nDb = strlen(argv[1]);
-    size_t nName = strlen(argv[2]);
-    size_t nFile = strlen(argv[3]);
-
-    size_t totalSize = sizeof(AvroTable) + nDb + nName + nFile + 3;
-
-    AvroTable *pAvro = sqlite3_calloc64(totalSize);
+    pAvro = sqlite3_malloc(sizeof(*pAvro));
     if (!pAvro) {
-        *pzErr = sqlite3_mprintf("Out of memory");
-        return SQLITE_NOMEM;
+        errMsg = sqlite3_mprintf("Out of memory");
+        rc = SQLITE_NOMEM;
+        goto error;
     }
+    memset(pAvro, 0, sizeof(*pAvro));
 
     pAvro->base.pModule = &avroModule;
-    pAvro->zDb = (char *)&pAvro[1];
-    pAvro->zName = &pAvro->zDb[nDb + 1];
-    pAvro->zFile = &pAvro->zName[nName + 1];
-    memcpy(pAvro->zDb, argv[1], nDb);
-    memcpy(pAvro->zName, argv[2], nName);
 
+    // set filename
+    size_t nFile = strlen(argv[3]);
+    pAvro->zFile = sqlite3_malloc64(nFile + 1);
+    if (!pAvro->zFile) {
+        errMsg = sqlite3_mprintf("Out of memory");
+        rc = SQLITE_NOMEM;
+        goto error;
+    }
+    // trim quotes
     if (argv[3][0] == '\'') {
         memcpy(pAvro->zFile, argv[3] + 1, nFile - 2);
         pAvro->zFile[nFile-2] = '\0';
@@ -166,37 +141,33 @@ int avroCreate(sqlite3 *db, void *pAux, int argc, const char *const*argv, sqlite
         memcpy(pAvro->zFile, argv[3], nFile);
     }
 
-    if (avro_file_reader(pAvro->zFile, &pAvro->reader)) {
-        sqlite3_free(pAvro);
-        *pzErr = sqlite3_mprintf("Failed to read Avro file: %s", avro_strerror());
-        return SQLITE_ERROR;
+    if (avro_file_reader(pAvro->zFile, &reader)) {
+        errMsg = sqlite3_mprintf("Failed to read Avro file: %s", avro_strerror());
+        rc = SQLITE_ERROR;
+        goto error;
     }
 
-    pAvro->schema = avro_file_reader_get_writer_schema(pAvro->reader);
-    pAvro->valueIface = avro_generic_class_from_schema(pAvro->schema);
+    pAvro->schema = avro_file_reader_get_writer_schema(reader);
+    pAvro->iface = avro_generic_class_from_schema(pAvro->schema);
 
     int nFields = avro_schema_record_size(pAvro->schema);
     if (nFields <= 0) {
-        *pzErr = sqlite3_mprintf("Avro schema has no fields");
-        avroTableFree(pAvro);
-        return SQLITE_ERROR;
+        errMsg = sqlite3_mprintf("Avro schema has no fields");
+        rc = SQLITE_ERROR;
+        goto error;
     }
 
-    char *createTable = sqlite3_mprintf("CREATE TABLE x(");
+    createTable = sqlite3_mprintf("CREATE TABLE x(");
     for (int i = 0; createTable && i < nFields; i++) {
         const char *fieldName = avro_schema_record_field_name(pAvro->schema, i);
         avro_schema_t fieldSchema = avro_schema_record_field_get_by_index(pAvro->schema, i);
 
-        const char *sqliteType = NULL;
-        rc = avroTypeToSqliteType(fieldSchema, &sqliteType, pzErr);
-        if (rc != SQLITE_OK) {
-            // error already set
-            sqlite3_free(createTable);
-            avroTableFree(pAvro);
-            return SQLITE_ERROR;
+        const char *sqliteType = avroTypeToSqliteType(fieldSchema);
+        if (!sqliteType) {
+            errMsg = sqlite3_mprintf("Unsupported Avro type for field '%s'", fieldName);
+            rc = SQLITE_ERROR;
+            goto error;
         }
-
-        avro_schema_decref(fieldSchema);
 
         const char *tail = (i+1 < nFields) ? ", " : ");";
         char *createTableNext = sqlite3_mprintf("%s %s %s %s", createTable, fieldName, sqliteType, tail);
@@ -205,125 +176,146 @@ int avroCreate(sqlite3 *db, void *pAux, int argc, const char *const*argv, sqlite
     }
 
     if (!createTable) {
-        *pzErr = sqlite3_mprintf("Out of memory");
-        avroTableFree(pAvro);
-        return SQLITE_NOMEM;
+        errMsg = sqlite3_mprintf("Out of memory");
+        rc = SQLITE_NOMEM;
+        goto error;
     }
 
     rc = sqlite3_declare_vtab(db, createTable);
-    sqlite3_free(createTable);
     if (rc != SQLITE_OK) {
-        *pzErr = sqlite3_mprintf("%s", sqlite3_errmsg(db));
-        avroTableFree(pAvro);
-        return SQLITE_ERROR;
+        errMsg = sqlite3_mprintf(sqlite3_errmsg(db));
+        // rc already set
+        goto error;
     }
 
+    goto end;
+error:
+    // cleanup resources that may have been allocated
+    avroDisconnect(&pAvro->base);
+    pAvro = NULL;
+
+end:
+    // cleanup local resources
+    sqlite3_free(createTable);
+    avro_file_reader_close(reader);
+
+    // set out parameters and return
     *ppVtab = (sqlite3_vtab *)pAvro;
-    *pzErr = NULL;
+    *pzErr = errMsg;
     return rc;
 }
 
-int avroConnect(sqlite3 *db, void *pAux, int argc, const char *const*argv, sqlite3_vtab **ppVtab, char **pzErr)
+static int avroConnect(sqlite3 *db, void *pAux, int argc, const char *const*argv, sqlite3_vtab **ppVtab, char **pzErr)
 {
     return avroCreate(db, pAux, argc, argv, ppVtab, pzErr);
 }
 
-int avroDisconnect(sqlite3_vtab *pVtab)
+/*
+ * Only full file-scan performed.
+ */
+static int avroBestIndex(sqlite3_vtab *pVtab, sqlite3_index_info *info)
 {
-    // TODO
     UNUSED(pVtab);
-    return SQLITE_OK;
-}
 
-int avroDestroy(sqlite3_vtab *pVtab)
-{
-    return avroDisconnect(pVtab);
-}
-
-int avroBestIndex(sqlite3_vtab *pVtab, sqlite3_index_info *info)
-{
-    /* Skipping for now */
-    UNUSED(pVtab);
-    UNUSED(info);
+    info->estimatedCost = 1000000;
 
     return SQLITE_OK;
 }
 
-static void avroSetError(AvroTable *pAvro, const char *fmt, ...)
-{
-    if (pAvro->base.zErrMsg) {
-        sqlite3_free(pAvro->base.zErrMsg);
-    }
+typedef struct AvroCursor {
+    sqlite3_vtab_cursor base;
+    avro_file_reader_t reader;
+    avro_value_t value;
+    int64_t row;
+    int eof;
+} AvroCursor;
 
-    va_list ap;
-    va_start(ap, fmt);
-    pAvro->base.zErrMsg = sqlite3_vmprintf(fmt, ap);
-    va_end(ap);
-}
-
-int avroOpen(sqlite3_vtab *pVtab, sqlite3_vtab_cursor **ppCursor)
+static int avroOpen(sqlite3_vtab *pVtab, sqlite3_vtab_cursor **ppCursor)
 {
     AvroTable *pAvro = (AvroTable *)pVtab;
 
-    AvroCursor *pCur = sqlite3_calloc(sizeof(AvroCursor));
+    AvroCursor *pCur = sqlite3_malloc(sizeof(*pCur));
     if (!pCur) {
-        avroSetError(pAvro, "Out of memory");
+        avroTableSetError(pAvro, "Out of memory");
         return SQLITE_NOMEM;
     }
+    memset(pCur, 0, sizeof(*pCur));
     pCur->base.pVtab = pVtab;
+
+    if (avro_file_reader(pAvro->zFile, &pCur->reader)) {
+        sqlite3_free(pCur);
+        avroTableSetError(pAvro, "Failed to open Avro file '%s' for reading.", pAvro->zFile);
+        return SQLITE_ERROR;
+    }
+    avro_generic_value_new(pAvro->iface, &pCur->value);
+    pCur->row = 0;
+    pCur->eof = 0;
 
     *ppCursor = (sqlite3_vtab_cursor *)pCur;
 
     return SQLITE_OK;
 }
 
-int avroClose(sqlite3_vtab_cursor *pVtabCursor)
+static int avroClose(sqlite3_vtab_cursor *pVtabCursor)
 {
     AvroCursor *pCur = (AvroCursor *)pVtabCursor;
-    sqlite3_free(pCur);
+
+    if (pCur) {
+        avro_file_reader_close(pCur->reader);
+        avro_value_decref(&pCur->value);
+        sqlite3_free(pCur);
+    }
+
     return SQLITE_OK;
 }
 
-int avroNext(sqlite3_vtab_cursor *pVtabCursor)
+static int avroNext(sqlite3_vtab_cursor *pVtabCursor)
 {
     AvroTable *pAvro = (AvroTable *)pVtabCursor->pVtab;
     AvroCursor *pCur = (AvroCursor *)pVtabCursor;
 
-    if (pAvro->eof) {
-        avroSetError(pAvro, "Can't advance to next, already at end of file.");
+    if (pCur->eof) {
+        avroTableSetError(pAvro, "Can't advance to next, already at end of file.");
         return SQLITE_ERROR;
     }
 
-    // avro_value_reset(&pAvro->value);
-    avro_generic_value_new(pAvro->valueIface, &pAvro->value);
+    // TODO: not using api correctly or a bug, just creating a new value every time
+    // avro_value_decref(&pCur->value);
+    // avro_value_reset(&pCur->value);
+    avro_generic_value_new(pAvro->iface, &pCur->value);
     pCur->row++;
 
     // advance to the next row
-    if (avro_file_reader_read_value(pAvro->reader, &pAvro->value)) {
-        pAvro->eof = true;
-        return SQLITE_OK;
+    if (avro_file_reader_read_value(pCur->reader, &pCur->value)) {
+        pCur->eof = 1;
     }
 
     return SQLITE_OK;
 }
 
-int avroFilter(sqlite3_vtab_cursor *pVtabCursor, int idxNum, const char *idxStr, int argc, sqlite3_value **argv)
+static int avroFilter(sqlite3_vtab_cursor *pVtabCursor, int idxNum, const char *idxStr, int argc, sqlite3_value **argv)
 {
-    AvroTable *pAvro = (AvroTable *)pVtabCursor->pVtab;
-    AvroCursor *pCur = (AvroCursor *)pVtabCursor;
-
-    // TODO
-    UNUSED(pCur);
     UNUSED(idxNum);
     UNUSED(idxStr);
     UNUSED(argc);
     UNUSED(argv);
 
-    // reset the file reader
-    pAvro->eof = false;
-    avro_file_reader_close(pAvro->reader);
-    avro_file_reader(pAvro->zFile, &pAvro->reader);
-    avro_generic_value_new(pAvro->valueIface, &pAvro->value);
+    AvroTable *pAvro = (AvroTable *)pVtabCursor->pVtab;
+    AvroCursor *pCur = (AvroCursor *)pVtabCursor;
+
+    avro_file_reader_close(pCur->reader);
+    avro_value_decref(&pCur->value);
+
+    if (avro_file_reader(pAvro->zFile, &pCur->reader)) {
+        avroTableSetError(pAvro, "Failed to open Avro file '%s'", pAvro->zFile);
+        return SQLITE_ERROR;
+    }
+    if (avro_generic_value_new(pAvro->iface, &pCur->value)) {
+        avroTableSetError(pAvro, "Failed to allocate new generic value");
+        return SQLITE_ERROR;
+    }
+    pCur->row = 0;
+    pCur->eof = 0;
 
     // read the first value
     avroNext(pVtabCursor);
@@ -331,36 +323,36 @@ int avroFilter(sqlite3_vtab_cursor *pVtabCursor, int idxNum, const char *idxStr,
     return SQLITE_OK;
 }
 
-int avroEof(sqlite3_vtab_cursor *pVtabCursor)
+static int avroEof(sqlite3_vtab_cursor *pVtabCursor)
 {
-    AvroTable *pAvro = (AvroTable *)pVtabCursor->pVtab;
-    return pAvro->eof;
+    AvroCursor *pCur = (AvroCursor *)pVtabCursor;
+
+    return pCur->eof;
 }
 
-int avroColumn(sqlite3_vtab_cursor *pVtabCursor, sqlite3_context *ctx, int idx)
+static int avroColumn(sqlite3_vtab_cursor *pVtabCursor, sqlite3_context *ctx, int idx)
 {
+    int rc = SQLITE_OK;
     AvroTable *pAvro = (AvroTable *)pVtabCursor->pVtab;
     AvroCursor *pCur = (AvroCursor *)pVtabCursor;
 
-    // TODO
-    UNUSED(pCur);
-    UNUSED(ctx);
-    UNUSED(idx);
-
-
     avro_schema_t fieldSchema = avro_schema_record_field_get_by_index(pAvro->schema, idx);
-
     avro_value_iface_t *fieldIface = avro_generic_class_from_schema(fieldSchema);
-    avro_value_t fieldValue;
-    avro_generic_value_new(fieldIface, &fieldValue);
 
-    const char *name = NULL;
-    if (avro_value_get_by_index(&pAvro->value, idx, &fieldValue, &name)) {
-        return SQLITE_ERROR;
+    avro_value_t fieldValue;
+    const char *fieldName = NULL;
+    if (avro_generic_value_new(fieldIface, &fieldValue)) {
+        avroTableSetError(pAvro, "Failed to allocate generic value");
+        rc = SQLITE_ERROR;
+        goto error;
+    }
+    if (avro_value_get_by_index(&pCur->value, idx, &fieldValue, &fieldName)) {
+        avroTableSetError(pAvro, "Failed to get field value from row");
+        rc = SQLITE_ERROR;
+        goto error;
     }
 
-    avro_type_t fieldType = avro_value_get_type(&fieldValue);
-    switch (fieldType) {
+    switch (avro_value_get_type(&fieldValue)) {
         case AVRO_STRING: {
             const char *str = NULL;
             size_t strSize = 0;
@@ -409,7 +401,7 @@ int avroColumn(sqlite3_vtab_cursor *pVtabCursor, sqlite3_context *ctx, int idx)
         case AVRO_BOOLEAN: {
             int b = 0;
             avro_value_get_boolean(&fieldValue, &b);
-            sqlite3_result_int(ctx, b); // not sure if this is correct
+            sqlite3_result_int(ctx, b);
             break;
         }
         // case AVRO_NULL:
@@ -420,17 +412,19 @@ int avroColumn(sqlite3_vtab_cursor *pVtabCursor, sqlite3_context *ctx, int idx)
         // case AVRO_UNION:
         default:
             // unsupported type
-            return SQLITE_ERROR;
-
+            avroTableSetError(pAvro, "Unsupported Avro value type");
+            rc = SQLITE_ERROR;
+            goto error;
     }
 
-    avro_schema_decref(fieldSchema);
+error:
     avro_value_decref(&fieldValue);
+    avro_value_iface_decref(fieldIface);
 
-    return SQLITE_OK;
+    return rc;
 }
 
-int avroRowid(sqlite3_vtab_cursor *pVtabCursor, sqlite_int64 *pRowid)
+static int avroRowid(sqlite3_vtab_cursor *pVtabCursor, sqlite_int64 *pRowid)
 {
     AvroCursor *pCur = (AvroCursor *)pVtabCursor;
 
@@ -439,14 +433,13 @@ int avroRowid(sqlite3_vtab_cursor *pVtabCursor, sqlite_int64 *pRowid)
     return SQLITE_OK;
 }
 
-
 static sqlite3_module avroModule = {
     0,                         /* iVersion */
     avroCreate,                /* xCreate */
     avroConnect,               /* xConnect */
     avroBestIndex,             /* xBestIndex */
     avroDisconnect,            /* xDisconnect */
-    avroDestroy,               /* xDestroy */
+    avroDisconnect,            /* xDestroy */
     avroOpen,                  /* xOpen - open a cursor */
     avroClose,                 /* xClose - close a cursor */
     avroFilter,                /* xFilter - configure scan constraints */
@@ -476,12 +469,12 @@ int sqlite3_avro_init(
     char **pzErrMsg,
     const sqlite3_api_routines *pApi
 ){
-    UNUSED(pzErrMsg); // TODO
-
     int rc = SQLITE_OK;
     SQLITE_EXTENSION_INIT2(pApi)
 
-    avroSetCustomAllocatorSqlite();
+    *pzErrMsg = NULL;
+    avro_set_allocator(avroSqliteAllocator, NULL);
+
     rc = sqlite3_create_module(db, "avro", &avroModule, NULL);
     return rc;
 }
